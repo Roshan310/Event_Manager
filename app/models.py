cc3e8b3723f3b544c03e53249963e6606c873bd4
Roshan Aryal <yuvrajaryal83@gmail.com>
2023-03-18 21:03:52 +0545

    add user table

--- app/models.py
+++ app/models.py
@@ -1,5 +1,15 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
 from .database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True, nullable=False)
+    email = Column(String, index=True, nullable=False)
+    password = Column(String, index=True, nullable=False)
+
+    event = relationship("Event", back_populates='owner')
 
 class Event(Base):
 
@@ -7,4 +17,4 @@ class Event(Base):
     id = Column(Integer, primary_key=True, index=True)
     event_name = Column(String, unique=True, index=True)
     event_details = Column(String, nullable=False)
-    
+    rsvp = Column(Boolean, nullable=False, default=False)
