9ef658efafdf3036d9983949ecbc010f0b6d0bbb
Roshan Aryal <yuvrajaryal83@gmail.com>
2023-03-22 22:56:37 +0545

    add user endpoint

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from typing import List
 from . import models, schemas
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -15,16 +16,18 @@ def get_db():
 
 models.Base.metadata.create_all(bind=engine)
 
-@app.get('/', tags=['test'])
+@app.get('/')
 def test():
     return {'msg': 'I am working fine!!!'}
 
-@app.get('/events')
+
+## Endpoints for Events
+@app.get('/events', tags=['Events'])
 def get_events(db: Session = Depends(get_db)):
     events = db.query(models.Event).all()
     return events
 
-@app.post('/add_event', response_model=schemas.Event)
+@app.post('/add_event', response_model=schemas.Event, tags=['Events'])
 def add_event(request: schemas.Event, db: Session = Depends(get_db)):
     new_event = models.Event(event_name = request.event_name, event_details = request.event_details)
     db.add(new_event)
@@ -32,4 +35,19 @@ def add_event(request: schemas.Event, db: Session = Depends(get_db)):
     db.refresh(new_event)
 
     return new_event
+
+## API endpoints for user
+@app.get('/get_users', response_model=List[schemas.User], tags=['User'])
+def get_user(db: Session = Depends(get_db)):
+    user = db.query(models.User).all()
+    return user
+
+@app.post('/create_user', response_model=schemas.User, tags=['User'])
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name = request.name, email = request.email, password = request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
 
