0f44de3a06264b983f802a08b4758ce1b713ae06
Roshan Aryal <yuvrajaryal83@gmail.com>
2023-06-02 21:16:32 +0545

    add verify_token function

--- app/authenticate.py
+++ app/authenticate.py
@@ -1,7 +1,6 @@
-import os
 from datetime import datetime, timedelta
 from typing import Union, Any
-from jose import jwt
+from jose import jwt, JWTError
 
 ACCESS_TOKEN_EXPIRES_MINUTES = 30 #30 Minutes
 REFRESH_TOKEN_EXPIRES_MINUTES = 60 * 24 * 7 #7 days
@@ -21,14 +20,9 @@ def create_access_token(subject: Union[str, Any], expires_delta: int = None):
 
     return jwt_encoded
 
-def create_refresh_token(subject: Union[str, Any], expires_delta: int = None):
-    if expires_delta is not None:
-        expires_delta = datetime.utcnow()  + expires_delta
-
-    else:
-        expires_delta = datetime.utcnow() + timedelta(minutes=REFRESH_TOKEN_EXPIRES_MINUTES)
-
-    to_encode = {'exp': expires_delta, 'sub': str(subject)}
-    jwt_encoded = jwt.encode(to_encode, JWT_REFRESH_KEY, algorithm=ALGORITHM)
-
-    return jwt_encoded
+def verify_access_token(token: str, credential_exception):
+    try:
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
+        id = payload.get('id')
+    except JWTError:
+        raise credential_exception
