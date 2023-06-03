aca4d6061cf08397d106a56cfff022758b44abe1
Roshan Aryal <yuvrajaryal83@gmail.com>
2023-06-03 12:18:51 +0545

    events now shows user info

--- app/schemas.py
+++ app/schemas.py
@@ -1,20 +1,43 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from typing import Optional
 
-class Event(BaseModel):
-    id: int
-    event_name: str
-    event_details: str
-
-    class Config:
-        orm_mode = True
-
-class User(BaseModel):
-    id: int
+class UserCreate(BaseModel):
     name: str
-    email: str
+    email: EmailStr
     password: str
 
+
+class UserOut(BaseModel):
+    name: str
+    email: str
+
     class Config:
         orm_mode = True
-        
 
+
+class EventCreate(BaseModel):
+    event_name: str
+    event_details: str
+    user_id: Optional[int]
+
+
+class EventShow(BaseModel):
+    event_name: str
+    event_details: str
+    created_by: UserOut
+
+    class Config:
+        orm_mode = True
+
+# class User(BaseModel):
+#     id: int
+#     name: str
+#     email: str
+#     password: str
+
+#     class Config:
+#         orm_mode = True
+
+        
+class TokenData(BaseModel):
+    id: Optional[str] = None
