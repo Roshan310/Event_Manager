e06100b700d9788bb947d1e7b008ac55ef57458b
Roshan Aryal <yuvrajaryal83@gmail.com>
2023-06-03 21:44:16 +0545

    add schema for Rsvp

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, conint
 from typing import Optional
 
 class UserCreate(BaseModel):
@@ -6,22 +6,21 @@ class UserCreate(BaseModel):
     email: EmailStr
     password: str
 
-
 class UserOut(BaseModel):
+    id: int
     name: str
     email: str
 
     class Config:
         orm_mode = True
 
-
 class EventCreate(BaseModel):
     event_name: str
     event_details: str
     user_id: Optional[int]
 
-
 class EventShow(BaseModel):
+    id: int
     event_name: str
     event_details: str
     created_by: UserOut
@@ -29,15 +28,9 @@ class EventShow(BaseModel):
     class Config:
         orm_mode = True
 
-# class User(BaseModel):
-#     id: int
-#     name: str
-#     email: str
-#     password: str
-
-#     class Config:
-#         orm_mode = True
-
-        
 class TokenData(BaseModel):
     id: Optional[str] = None
+
+class Rsvp(BaseModel):
+    event_id: int
+    dir: conint(le=1)
