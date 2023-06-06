fa01fdfe3a662fc8b64c41e02fffe75e4790286e
Roshan Aryal <yuvrajaryal83@gmail.com>
2023-06-06 21:30:33 +0545

    add description to the api

--- app/main.py
+++ app/main.py
@@ -3,7 +3,29 @@ from . import models
 from .database import engine
 from .routers import events, users, auth, rsvps
 
-app = FastAPI()
+description = """
+Event manager API helps you do awesome stuff. 🚀
+
+## Events
+
+You can create different events.
+You can see upcoming events.
+
+## RSVP
+You will be able to register for the events.
+
+## Users
+
+You will be able to:
+
+* **Create users** .
+* **Read users** .
+"""
+
+app = FastAPI(
+    title='Event Manager',
+    description=description
+)
 
 
 models.Base.metadata.create_all(bind=engine)
